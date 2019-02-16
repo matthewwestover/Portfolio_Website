d1e1c5a4e47e89d769fffed448bbb850d31d596b
Matt Westover <matthew.westover@gmail.com>
2019-02-15 23:02:35 -0700

    set App.js to check for mobile device for app rendering

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
+import {isMobile} from 'react-device-detect';
 import ScrollToTop from './components/global/ScrollToTop';
 import Home from './components/Home';
 import NoMatch from './components/NoMatch';
 import Navbar from './components/global/Navbar'
 import Footer from './components/global/Footer';
+import MobileHome from './mobile/MobileHome';
 import './App.css';
 
 class App extends React.Component {
+
+  renderContent = () => {
+    if (isMobile) {
+      return(
+        <Fragment>
+          <Route exact path='/' component={MobileHome} />
+        </Fragment>
+      )
+    } else {
+      return (
+        <Fragment>
+          <Navbar />
+          <Container fluid>
+            <ScrollToTop>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route component={NoMatch} />
+              </Switch>
+            </ScrollToTop>
+          </Container>
+          <Footer />
+        </Fragment>
+      )
+    }
+  }
+
   render() {
     return (
-      <Fragment>
-        <Navbar />
-        <Container fluid>
-          <ScrollToTop>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route component={NoMatch} />
-            </Switch>
-          </ScrollToTop>
-        </Container>
-        <Footer />
-      </Fragment>
+      this.renderContent()
     );
   }
 }
